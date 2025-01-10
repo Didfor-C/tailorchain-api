137f30c0e7c69af350a25d4b81e8ef00e0728ead
D-Delante <83444466+D-Donce@users.noreply.github.com>
2025-01-10 11:56:34 +0900

    Setting the Heroku Dedicated Server Port Number

--- index.js
+++ index.js
@@ -66,8 +66,9 @@ async function main() {
   });
 
   // 서버 실행
-  app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
   });
 }
 
